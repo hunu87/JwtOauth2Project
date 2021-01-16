f005dd5d1dffd37dfb4faa9349bf77b45aca9dba
이현우 <ihyeon-u@ihyeon-uui-MacBookPro.local>
2021-01-16 22:35:05 +0900

    modify info message

--- JwtOauth2Project/src/main/java/com/kiwoom/demo/manage/security/service/CustomUserDetailService.java
+++ JwtOauth2Project/src/main/java/com/kiwoom/demo/manage/security/service/CustomUserDetailService.java
@@ -11,8 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 	
-	// 사용자 아이디 패스워드를 확인하는 로직이 들어 가야 
-	
+	// 원래는 사용자 아이디 패스워드를 확인하는 로직이 들어 가야 함
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return new User("user", "1234", new ArrayList<>());
